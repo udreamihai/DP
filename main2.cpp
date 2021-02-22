6d768d69a2f319946f0241843a1883697103009f
Mihai Udrea <udreamihai@gmail.com>
2021-02-22 10:26:23 +0000

    getting there

--- main2.cpp
+++ main2.cpp
@@ -20,64 +20,13 @@
 #include <sys/types.h>
 
 #include <ctime>
+#include <sys/wait.h>
+#define NUM_OF_CORES 12
+#define MAX_PRIME 1000000
 
 using namespace std;
 
-void xxx(char* buffer, int sz){
-  string filename("out.bin");
-  vector<char> bytes;
-  FILE* input_file = fopen(filename.c_str(), "r");
-  int cursor = 0;//parse the exif_file
-  char exif_file[] = {};
-  unsigned char character = 0;
-  while (!feof(input_file)){
-    character = getc(input_file);
-    exif_file[cursor] = character;
-    cursor++;
-  }
-  exif_file[cursor] = '\0';
-  for (int n=0; n<sizeof(exif_file); n++){
-    buffer[n] = exif_file[n];
-  }
-  fclose(input_file);
-}
-
-
-int prime(int a, int b){
-    int primes = 0;
-      for (a; a <= b; a++) {
-          int i = 2;
-          while (i <= a) {
-              if (a % i == 0)
-                  break;
-              i++;
-          }
-          if (i == a) {
-              primes++;
-          }
-      }
-    return primes;
-}
-
-
-int workConsumingPrime(vector<pair<int, int>>& workQueue, mutex& workMutex)
-{
-    int primes = 0;
-    unique_lock<mutex> workLock(workMutex);
-    while (!workQueue.empty()) {
-        pair<int, int> work = workQueue.back();
-        workQueue.pop_back();
-
-        workLock.unlock(); //< Don't hold the mutex while we do our work.
-        primes += prime(work.first, work.second);
-
-        pid_t tid;
-        tid = syscall(SYS_gettid);
-        cout << "Process id " << tid << "\n";
-        workLock.lock();
-    }
-    return primes;
-}
+void primes();
 
 int main(){
   string myString = "";
@@ -125,45 +74,42 @@ int main(){
   }
   cout << "\n";
 // all good up to this point
-  int start_s=clock();
+  time_t start, end;
+  time_t run_time;
+  unsigned long i;
+  pid_t pids[NUM_OF_CORES];
 
-
-  int nthreads = thread::hardware_concurrency();
-  //int nthreads = 2;
-      int limit = 1000000;
-
-      // A place to put work to be consumed, and a synchronisation object to protect it.
-      vector<pair<int, int>> workQueue;
-      mutex workMutex;
-
-      // Put all of the ranges into a queue for the threads to consume.
-      int chunkSize = max(limit / (nthreads*16), 10); //< Handwaving came picking 16 and a good factor.
-      for (int i = 0; i < limit; i += chunkSize) {
-          workQueue.push_back(make_pair(i, min(limit, i + chunkSize)));
+  /* start of test */
+  start = time(NULL);
+  for (i = 0; i < NUM_OF_CORES; ++i) {
+      if (!(pids[i] = fork())) {
+          primes();
+          exit(0);
       }
-
-
-
-        // Start the threads.
-        vector<future<int>> futures;
-        for (int i = 0; i < nthreads; ++i) {
-            packaged_task<int()> task(bind(workConsumingPrime, ref(workQueue), ref(workMutex)));
-            futures.push_back(task.get_future());
-            thread(move(task)).detach();
-        }
-
-
-      cout << "Number of logical cores used: " << nthreads << "\n";
-      cout << "Calculating number of primes less than " << limit << "... \n";
-
-      // Sum up all the results.
-      int primes = 0;
-      for (future<int>& f : futures) {
-          primes += f.get();
+      if (pids[i] < 0) {
+          perror("Fork");
+          exit(1);
       }
-      int stop_s=clock();
-      std::cout << "\n... and it only took " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << " seconds." << std::endl;
-      //cout << "There are " << primes << " prime numbers less than " << limit << ".\n";
+  }
+  for (i = 0; i < NUM_OF_CORES; ++i) {
+      waitpid(pids[i], NULL, 0);
+  }
+  end = time(NULL);
+  run_time = (end - start);
+  printf("This machine calculated all prime numbers under %d %d times "
+         "in %d seconds\n", MAX_PRIME, NUM_OF_CORES, run_time);
+  return 0;
 
-
+}
+//calculate prime numbers
+void primes()
+{
+    unsigned long i, num, primes = 0;
+    cout << "process id " << getpid() << "\n";
+    for (num = 1; num <= MAX_PRIME; num++) {
+        for (i = 2; (i <= num) && (num % i != 0); i++);
+        if (i == num)
+            primes++;
+    }
+    printf("Calculated %d primes.\n", primes);
 }
